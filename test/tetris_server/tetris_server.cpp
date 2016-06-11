e71bf88f1d83e7b95f87b61eddb0b6b741ed928c
guangleihe <guangleihe@gmail.com>
2016-06-11 22:11:26 +0800

    add opt for tetris_server

--- test/tetris_server/tetris_server.cpp
+++ test/tetris_server/tetris_server.cpp
@@ -2,10 +2,28 @@
 // All rights reserved
 #define CS_NDEBUG
 #include "server/tetris_web_server.h"
+#include <iostream>
+#include "vzflags.h"
 
 
+void PrintUsage() {
+  printf("\n");
+  printf("\n");
+  printf("  usage:\n");
+  printf("\n");
+  printf(
+    "        -e  <logtostderr>     Defualt value is 0 (google::GLOG_INFO)\n");
+  printf(
+    "        -m  <miniloglevel>    Default value is 2 (google::GLOG_ERROR)\n");
+  printf(
+    "        -v                    Verbose Mode\n");
+  printf("\n");
+  printf(
+    "       [-h]                   print usage\n");
+  printf("\n");
+}
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
   tetris::TetrisServerSettings tss;
 
@@ -13,6 +31,32 @@ int main(void) {
   tss.http_port = "8000";
   tss.html_root = "F:/code/osc/TetrisGame/test/tetris_server/web_root";
   tss.database_path = "F:/code/osc/TetrisGame/test/tetris_server/rank_database.db";
+
+  int opt = 0;
+  while((opt = getopt(argc, argv, "a:p:r:d:h")) != -1) {
+    switch (opt) {
+    case 'a':
+      tss.bind_addr = optarg;
+      break;
+    case 'p':
+      tss.http_port = atoi(optarg);
+      break;
+    case 'r':
+      tss.html_root = optarg;
+      break;
+    case 'd':
+      tss.database_path = optarg;
+      break;
+    case 'h':
+      PrintUsage();
+      exit(EXIT_SUCCESS);
+    }
+  }
+  
+  std::cout << tss.bind_addr << std::endl;
+  std::cout << tss.http_port << std::endl;
+  std::cout << tss.html_root << std::endl;
+  std::cout << tss.database_path << std::endl;
 
   tetris::TetrisWebServer server(tss);
 
---
+++ test/tetris_server/vzflags.h
@@ -0,0 +1,14 @@
+#ifndef VZBASE_FLAGS_VZFLAGS_H_
+#define VZBASE_FLAGS_VZFLAGS_H_
+
+#ifdef WIN32
+#include "getopt.h"
+#else
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#endif
+
+#endif  // VZBASE_FLAGS_VZFLAGS_H_
